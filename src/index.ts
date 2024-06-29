42333853c74c3c27b5e71b59ac2096c7ebcd0e81
Sova <108209320+NceSova@users.noreply.github.com>
2024-06-29 16:43:08 +0300

    add giphy api calls and option to search

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
 import "./index.css";
+
+const API_KEY = "hO0hgMqv01petXEKhgJbcKSFW91GYQuo";
+
+const img = document.querySelector("img");
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+button.addEventListener("click", (e) => {
+  const inputText = input.value;
+  input.value = "";
+  fetchGif(inputText);
+});
+function fetchGif(search: string) {
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${search}&weirdness=10`,
+    {mode: "cors"}
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      img.src = response.data.images.original.url;
+    })
+    .catch((error) => {
+      console.log(error);
+      alert(error);
+    });
+}
+fetchGif("");
